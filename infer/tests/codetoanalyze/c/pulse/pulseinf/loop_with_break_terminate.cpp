616307e0480e49d8c9b86b158da24939f6f1713d
Julien Vanegue <jvanegue@bloomberg.net>
2024-06-15 19:34:58 -0400

    More independent tests

---
+++ infer/tests/codetoanalyze/c/pulse/pulseinf/loop_with_break_terminate.cpp
@@ -0,0 +1,9 @@
+/* pulse-inf: works good! no bug */
+void loop_with_break_terminate(int y) {
+  y = 0;
+  while (y < 100)
+    if (y == 50)
+      break;
+    else
+      y++;
+}
--- infer/tests/codetoanalyze/c/pulse/pulseinf/simple_loop_not_terminate.cpp
+++ infer/tests/codetoanalyze/c/pulse/pulseinf/simple_loop_not_terminate.cpp
@@ -1,10 +1,9 @@
 /* pulse inf works if x is a parameter */
 /* pulse inf DOES NOT work (FN) if x is a local variable */
-
-//int f();
-
-void simple_loop_not_terminate(int x) {
-  int y = 0;
+int simple_loop_not_terminate() {
+  int x = 1;
+  int y = 2;
   while (x != 3)
     y++;
+  return (y);
 }
