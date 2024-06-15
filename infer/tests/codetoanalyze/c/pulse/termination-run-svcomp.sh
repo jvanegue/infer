f000b4760c696a1ac58183ad8b0d06cefee19eb4
Julien Vanegue <jvanegue@bloomberg.net>
2024-06-15 19:15:14 -0400

    Added dedicated file for svcomp (nla) testing

---
+++ infer/tests/codetoanalyze/c/pulse/termination-run-svcomp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This is for Julien's setup - you can do like me and store your build tree somewhere with a lot of space.
+# Change this to point to your infer build tree
+# You can disable this if you build infer from your real home folder
+HOME=/huge/jvanegue/PUBLIC_GITHUB/infer
+
+rm -fr infer-out/ infinite.o infer-run.log
+
+#
+# Enable for SV-COMP non-linear arithmetic tests
+#
+rm -fr pulseinf-svcomp-logs
+mkdir pulseinf-svcomp-logs
+for i in `ls pulseinf/svcomp-nla/*.c`; do
+    echo Running infer on $i
+    NEWNAME=`echo $i | sed s/pulseinf.svcomp.nla.//g`
+    echo Short name for test: $NEWNAME
+    $HOME/infer/bin/infer run --debug-level=0 --pulse-only -- clang++ -c "$i" 2> pulseinf-svcomp-logs/infer-run-"$NEWNAME".log
+    python3 -m json.tool infer-out/report.json > pulseinf-svcomp-logs/pulseinf-report-"$NEWNAME".json
+done
