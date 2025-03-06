c46c01ac60dec13f22ad5465843951f3ca736f04
Julien Vanegue <jvanegue@bloomberg.net>
2025-03-06 06:05:38 -0500

    Cleaned up pulse execution domain module for (PR prep)

--- infer/src/pulse/PulseExecutionDomain.ml
+++ infer/src/pulse/PulseExecutionDomain.ml
@@ -107,12 +107,7 @@ let pp fmt (exec_state: 'abductive_domain_t base_t) =
  F.fprintf fmt "%a%a" (pp_header TEXT) exec_state AbductiveDomain.pp (to_astate exec_state)
 
 (* Pulse infinite *)
-(* We record the global widening state *)
-(* Note: this will record the widen state once per analyzed object, which may not be enough *)
-(* TODO: Find out how to have one widen state key per loop *)                      
-(* let widenstate = ref None;; 
-let () = AnalysisGlobalState.register_ref ~init:(fun () -> Some (Stdlib.Hashtbl.create 16)) widenstate;; *)
-
+  
 let widenstate = AnalysisGlobalState.make_dls ~init:(fun () -> (Some (Stdlib.Hashtbl.create 16)))
 
 let get_widenstate () = DLS.get widenstate
@@ -127,13 +122,6 @@ let back_edge (prev: t list) (next: t list) (num_iters: int)  : t list * int =
   (* L.debug Analysis Quiet "PULSEINF: Entered BACK-EDGE! \n"; *)
   
   let _ = num_iters in
-  
-  (* Instead of this, we want to stop reporting at current and next widening iteration 
-     if we already have had an alert at a previous widening iteration *)
-  (* if (num_iters <= 0) then next,-1 else *)
-  (* It looks like infer reporting will filter issues by location to avoid duplicate already *)
-
-  (* let cfgnode = (Metadata.get_cfg_node ()) in  *)
 
   let cfgnode = AnalysisState.get_node_exn() in
   
@@ -150,10 +138,7 @@ let back_edge (prev: t list) (next: t list) (num_iters: int)  : t list * int =
     | [] -> newset
     | hd::tl ->
        let res,idx = detect_elem hd prev 0 in
-       (* L.debug Analysis Quiet "JV: compute_workset find res = %b idx %u \n" res idx; *)
-
-       (* record the idx of the new state in postcond (next) *)
-       let recidx = (match idx with | -1 -> nextit | _ -> idx)
+        let recidx = (match idx with | -1 -> nextit | _ -> idx)
        in
        if (res)
        then (compute_workset prev tl newset (nextit + 1))
@@ -211,27 +196,6 @@ let back_edge (prev: t list) (next: t list) (num_iters: int)  : t list * int =
     | ContinueProgram astate -> AbductiveDomain.get_path_condition astate                             
     | LatentSpecializedTypeIssue {astate; _} -> AbductiveDomain.Summary.get_path_condition astate
   in
-  
-  (* Used when the workset is empty and we need to select one common state *)
-  (*
-  let rec detect_common_elem_with_non_null_formula prev next : int =
-    match next with
-    | [] -> -1
-    | hd::tl ->
-       let (found,idx) = (detect_elem hd prev 0) in
-       match found with
-       | false -> (detect_common_elem_with_non_null_formula prev tl)
-       | true ->
-          let cond = extract_pathcond hd in
-          let fempty = Formula.formula_is_empty cond in
-          L.debug Analysis Quiet "PULSEINF: TRIVIAL LASSO with EMPTY WORKSET (P:%u N:%u W:%u) - fempty:%b numiter:%u \n"
-            prevlen nextlen worklen fempty num_iters;
-          L.flush_formatters();
-          (Formula.pp Format.err_formatter cond);
-          L.flush_formatters();
-          if (fempty) then (detect_common_elem_with_non_null_formula prev tl) else idx
-  in
-  *)
 
   (* Used when the workset is not empty *)
   let rec record_pathcond ws : int =
@@ -240,55 +204,33 @@ let back_edge (prev: t list) (next: t list) (num_iters: int)  : t list * int =
     match ws with
     | [] -> (-1)
     | (hd,idx)::tl ->
-       (* let wstate = get_widenstate in *)
-       (* | None -> L.debug Analysis Quiet "PULSEINF: widenstate htable NONE - should never happen! num_iter %u \n" num_iters; -1 *)
-       (*| Some wstate -> *)
-          let cond = extract_pathcond hd in
-          let pathcond = Formula.extract_path_cond cond in
-          let termcond = Formula.extract_term_cond cond in
-          let termcond2 = Formula.extract_term_cond2 cond in
-          let dl_ws = get_widenstate () in
-
-          match dl_ws with
-
-          | None -> L.debug Analysis Quiet "PULSEINF: widenstate htable NONE - should never happen! num_iter %u \n" num_iters; -1 
-          | Some ws ->
-          
-          let prevstate = (* (if true then *)
-                             (Stdlib.Hashtbl.find_opt ws (cfgnode,termcond,pathcond,termcond2))
-                               (* Old mode - more FP *)
-                           (* else
-                             (Stdlib.Hashtbl.find_opt get_wstate (cfgnode,Formula.Atom.Set.empty,pathcond,Formula.Term.Set.empty))) *)
-                        
-          in
-          match prevstate with
-          | None ->
-             let key = (* (if true then *)
-                          (cfgnode,termcond,pathcond,termcond2)
-                                       (* Old mode - more FP *)
-                            (* else (cfgnode,Formula.Atom.Set.empty,pathcond,Formula.Term.Set.empty)) *)
-             in
-             (* L.debug Analysis Quiet "PULSEINF: Recorded pathcond NOT in htable (ADDING) idx %d numiter %u \n" idx num_iters; *)
-             let _ = add_widenstate key in
-             record_pathcond tl
-          | Some _ ->
-             match (Formula.set_is_empty termcond),(Formula.map_is_empty pathcond),(Formula.termset_is_empty termcond2) with 
-             | true,true,true -> -2
-             (* match (Formula.set_is_empty pathcond) with
-             | true -> *)
-             (* L.debug Analysis Quiet "PULSEINF: Recorded pathcond ALREADY in htable! (EMPTY) idx %d numiter %u \n" idx num_iters; -2 *)
-             | _ -> idx
+       let cond = extract_pathcond hd in
+       let pathcond = Formula.extract_path_cond cond in
+       let termcond = Formula.extract_term_cond cond in
+       let termcond2 = Formula.extract_term_cond2 cond in
+       let dl_ws = get_widenstate () in
+       match dl_ws with
+       | None -> L.debug Analysis Quiet "PULSEINF: widenstate htable NONE - should never happen! num_iter %u \n" num_iters; -1 
+          | Some ws ->           
+             let prevstate = (Stdlib.Hashtbl.find_opt ws (cfgnode,termcond,pathcond,termcond2)) in
+             match prevstate with
+             | None ->
+                let key = (cfgnode,termcond,pathcond,termcond2) in
+                (* L.debug Analysis Quiet "PULSEINF: Recorded pathcond NOT in htable (ADDING) idx %d numiter %u \n" idx num_iters; *)
+                let _ = add_widenstate key in
+                record_pathcond tl
+             | Some _ ->
+                match (Formula.set_is_empty termcond),(Formula.map_is_empty pathcond),(Formula.termset_is_empty termcond2) with 
+                | true,true,true -> -2
                 (* L.debug Analysis Quiet "PULSEINF: Recorded pathcond ALREADY in htable! (NON-TERM BUG) idx %d numiter %u \n" idx num_iters; idx *)
+                | _ -> idx
+                     
   in                                                                                                                                   
   
   (* let _ = print_workset workset in *)
   let (repeated_wsidx:int) =
     if (phys_equal same true) then (-1)
-    else
-      (
-        (* if (phys_equal worklen 0) then detect_common_elem_with_non_null_formula prev next else else *)
-        record_pathcond workset
-      )
+    else record_pathcond workset
   in
   
   let create_infinite_state (hd:t) (cnt:int) : t =
@@ -317,10 +259,6 @@ let back_edge (prev: t list) (next: t list) (num_iters: int)  : t list * int =
        [(create_infinite_state state idx)],idx
   in
   
-  (* let lastelm = (List.length next) in *)
-  (* let idx = (if lastelm > 0 then lastelm-1 else 0) in *)
-  (* let isempty = (phys_equal lastelm 0) || (phys_equal (List.length prev) 0) in 
-  let prevempty = (phys_equal (List.length prev) 0) in *)
   let nextempty = (phys_equal (List.length next) 0) in 
   
   (* Identify which state in the post is repeated and generate a new infinite state for it *)
@@ -333,29 +271,16 @@ let back_edge (prev: t list) (next: t list) (num_iters: int)  : t list * int =
     match lst with
     | hd::tl -> let isrep = (is_repeated hd tl) in
                 (* L.debug Analysis Quiet "JV: Found duplicate state in POST with index = %d \n" curi; *)
-                if (isrep && curi > maxi) then
-                  (find_duplicate_state tl (curi+1) curi)
-                else
-                  (find_duplicate_state tl (curi+1) maxi)
+                if (isrep && curi > maxi) then (find_duplicate_state tl (curi+1) curi)
+                else (find_duplicate_state tl (curi+1) maxi)
     | _ -> (* L.debug Analysis Quiet "JV: Chosen duplicate in POST has index = %d \n" maxi; *) maxi 
   in  
 
-  (* let print_empty_warning prevempty nextempty =
-    L.debug Analysis Quiet "JV: BUG MAYBE FOUND prevempty=%b nextempty=%b \n" prevempty nextempty;
-    [],-1
-  in *)
-  
-  (* if same && (phys_equal isempty true) then
-    print_empty_warning prevempty nextempty *)
   (* we have a trivial lasso because prev = next - this should trigger an alert *)
   if same && (phys_equal nextempty false) then
     (* (L.debug Analysis Quiet "PULSEINF: same && nextempty is true - returning (no bug) \n"; *)
-    (* [],-1 *)
     create_infinite_state_and_print next (find_duplicate_state next 0 (-1)) 1
   
-  (* else if (phys_equal worklen 0) && (repeated_wsidx >= 0) then
-   c reate_infinite_state_and_print next repeated_wsidx 1 *)
-
   (* We have one or more newly created equivalent states in the post, trigger an alert *)
   else if (phys_equal worklen 0) && (nextlen - prevlen) > 0 then
     create_infinite_state_and_print next (find_duplicate_state next 0 (-1)) 2
