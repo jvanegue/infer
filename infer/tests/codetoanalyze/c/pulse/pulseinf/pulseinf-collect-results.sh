57abe86150ff236b1ed0f47c91977aaa936754a3
Julien Vanegue <jvanegue@bloomberg.net>
2025-04-14 23:27:53 -0400

    Changed in collect results script

--- infer/tests/codetoanalyze/c/pulse/pulseinf/pulseinf-collect-results.sh
+++ infer/tests/codetoanalyze/c/pulse/pulseinf/pulseinf-collect-results.sh
@@ -2,7 +2,7 @@
 
 mkdir -p results
 
-for i in bde bitcoin comdb2 FreeImage libpng libxpm mbedtls openssl zlib bind9 blazingmq cryptopp gpac libxml2 open5gs pcre2 proftpd sqlite lua
+for i in bde bitcoin FreeImage libpng libxpm mbedtls openssl zlib bind9 blazingmq cryptopp gpac libxml2 open5gs pcre2 proftpd sqlite lua
 do
     cp projects/$i/infer-log*.log ./results/
     cp projects/$i/report-indented.json ./results/pulseinf-report-$i.json
@@ -17,11 +17,15 @@ cp projects/libexpat/expat/infer-log*.log ./results/
 cp projects/libexpat/expat/report-indented.json ./results/pulseinf-report-libexpat.json
 
 # These are under src/ subfolder for LIBGIT2
-cp projects/libgit2/build/infer-log*.log ./results/pulseinf-report-libgit2.json
-cp projects/libgit2/build/report-indented.json ./results/
+cp projects/libgit2/build/infer-log*.log ./results/
+cp projects/libgit2/build/report-indented.json ./results/pulseinf-report-libgit2.json
 
 # These are under build/ subfolder for WIRESHARK
-cp projects/wireshark/build/infer-log*.log ./results/pulseinf-report-wireshark.json
-cp projects/wireshark/build/report-indented.json ./results/
+cp projects/wireshark/build/infer-log*.log ./results/
+cp projects/wireshark/build/report-indented.json ./results/pulseinf-report-wireshark.json
+
+# These are under build/ subfolder for COMDB2
+cp projects/comdb2/build/infer-log*.log ./results/
+cp projects/comdb2/build/report-indented.json ./results/pulseinf-report-comdb2.json
 
 echo Finished collecting logs. Check results folder for results.
